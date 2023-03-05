9924ff2dc38c734f7e8c5783fb948723547aee93
ana <anaisabelmartins@tecnico.ulisboa.pt>
2023-03-05 15:10:30 +0100

    new parameters

--- src/plot.py
+++ src/plot.py
@@ -6,7 +6,7 @@ from lennard_jones import lennard_jones
 epsilon = 1.66e-21
 sigma = 3.4e-10
 
-x = np.arange(-1, 1, 0.1)
+x = np.arange(-0.1, 0.1, 0.001)
 
 plt.plot(x, lennard_jones(x, epsilon, sigma))
 plt.xlabel('r')
